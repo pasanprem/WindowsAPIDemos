d76aba716c473d3c253a0f0e10e7752087f0bd6c
Pasan Premaratne <pasan@pasandev.com>
2021-09-03 13:26:48 +0100

    Some more stuff

--- Hello/Hello.cpp
+++ Hello/Hello.cpp
@@ -3,6 +3,7 @@
 
 #include <Windows.h>
 #include <stdio.h>  
+#include <strsafe.h>
 
 int Error(const char* msg) {
     printf("%s (%u)\n", msg, ::GetLastError());
@@ -18,13 +19,26 @@ int main()
     printf("Page size: %u\n", si.dwPageSize);
     printf("Processor mask: 0x%zX\n", si.dwActiveProcessorMask);
 
-    HANDLE hProcess = ::OpenProcess(PROCESS_TERMINATE, FALSE, 12988);
+    /*HANDLE hProcess = ::OpenProcess(PROCESS_TERMINATE, FALSE, 12988);
     if (hProcess) {
         ::TerminateProcess(hProcess, 0);
     }
     else {
         return Error("Failed to open process");
-    }
+    }*/
+
+    WCHAR buffer[128];
+    ::StringCchPrintf(buffer, _countof(buffer), L"This is my string from process %u", ::GetCurrentProcessId());
+    //::MessageBox(nullptr, buffer, L"Strings demo", MB_OK | MB_ICONINFORMATION);
+
+    WCHAR path[MAX_PATH];
+    ::GetSystemDirectory(path, _countof(path));
+    printf("System directory: %ws\n", path);
+
+    WCHAR computerName[MAX_COMPUTERNAME_LENGTH];
+    DWORD len = _countof(computerName);
+    if (::GetComputerName(computerName, &len))
+        printf("Computer name: %ws (%u)\n", computerName, len);
 
     return 0;
 }
